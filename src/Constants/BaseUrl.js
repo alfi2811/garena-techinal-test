91432bdcbe391d5190051e680441cba4116b4ce7
alfi2811 <alfinasution64@gmail.com>
2020-11-26 09:14:32 +0700

    put API in .env

--- src/Constants/BaseUrl.js
+++ src/Constants/BaseUrl.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export default axios.create({ baseURL: 'https://form-v2.support.garena.co.id/_/items' })
+export default axios.create({ baseURL: process.env.REACT_APP_API_URL })
